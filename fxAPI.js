4a5d93d5e33bac435f34a203a5add0977835f553
Kidai <deznevnikita8@gmail.com>
2026-07-27 02:23:52 +0300

    Add comments to fxAPI.js functions

--- fxAPI.js
+++ fxAPI.js
@@ -1,11 +1,11 @@
 fx = {
-    pushString: function(data) {
+    pushString: function(data) { // pushes the string into local IPC
         window.webkit.messageHandlers.busMessage.postMessage(JSON.stringify(data));
     },
-    recalculateInputRegions: function() {
+    recalculateInputRegions: function() { // SIRs
         window.webkit.messageHandlers.busMessage.postMessage(JSON.stringify({fxAPICall: {RIR: true}}));
     },
-    closeApplication: function() {
+    closeApplication: function() { // CLOSE
         window.webkit.messageHandlers.busMessage.postMessage(JSON.stringify({fxAPICall: {CLOSE: true}}));
     }
 };
