783a6153e80248be3d4d0fefbb3b35445166df33
Kidai <deznevnikita8@gmail.com>
2026-07-26 23:08:21 +0000

    Update fxAPI.js

--- fxAPI.js
+++ fxAPI.js
@@ -1,5 +1,11 @@
 fx = {
     pushString: function(data) {
         window.webkit.messageHandlers.busMessage.postMessage(JSON.stringify(data));
+    },
+    recalculateInputRegions: function() {
+        window.webkit.messageHandlers.busMessage.postMessage(JSON.stringify({fxAPICall: {RIR: true}}));
+    },
+    closeApplication: function() {
+        window.webkit.messageHandlers.busMessage.postMessage(JSON.stringify({fxAPICall: {CLOSE: true}}));
     }
 };
