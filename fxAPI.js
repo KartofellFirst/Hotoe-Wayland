c8b42c71f98b52663ed6f3136f73f11a0ada5592
Kidai <deznevnikita8@gmail.com>
2026-07-27 23:49:49 +0300

    Add requestFileContent method to fxAPI

--- fxAPI.js
+++ fxAPI.js
@@ -7,5 +7,40 @@ fx = {
     },
     closeApplication: function() { // CLOSE
         window.webkit.messageHandlers.busMessage.postMessage(JSON.stringify({fxAPICall: {CLOSE: true}}));
+    },
+    requestFileContent: function(filePath) { // read(path).then(file => log(file.content))
+        return this._callWithPromise("readFile", { filePath: filePath });
+    },
+
+    
+    // system-reserved fields
+    _promises: {},
+    _callCounter: 0,
+    _callWithPromise: function(action, payload = {}) {
+        return new Promise((resolve, reject) => {
+            const callId = "call_" + (++this._callCounter) + "_" + Date.now();
+            this._promises[callId] = { resolve, reject };
+
+            const message = {
+                fxAPICall: {
+                    action: action,
+                    callId: callId,
+                    payload: payload
+                }
+            };
+            window.webkit.messageHandlers.busMessage.postMessage(JSON.stringify(message));
+        });
+    },
+    _resolvePromise: function(callId, error, result) {
+        if (!this._promises[callId]) return;
+        
+        const { resolve, reject } = this._promises[callId];
+        delete this._promises[callId];
+
+        if (error) {
+            reject(new Error(error));
+        } else {
+            resolve(result);
+        }
     }
 };
